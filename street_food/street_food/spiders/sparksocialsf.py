7187b3eb2f2707b1d4f74a4bde1fc294ec1705aa
kirimaks <kirimaks@yahoo.com>
2016-10-04 21:08:46 +0300

    sparksocialsf scraper

--- street_food/street_food/spiders/sparksocialsf.py
+++ street_food/street_food/spiders/sparksocialsf.py
@@ -1,13 +1,59 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy import Request
+from street_food.items import StreetFoodDatTimeItem
+import street_food.tools.sparksocialsf_tools as tools
 
 
 class SparksocialsfSpider(scrapy.Spider):
     name = "sparksocialsf"
-    allowed_domains = ["sparksocialsf.com/schedule"]
-    start_urls = (
-        'http://www.sparksocialsf.com/schedule/',
-    )
+    allowed_domains = ["sparksocialsf.com"]
 
-    def parse(self, response):
-        pass
+    def start_requests(self):
+        url = 'http://www.sparksocialsf.com/schedule/'
+
+        yield Request(url, callback=self.parse_lunch_vendors)
+        yield Request(url, callback=self.parse_dinner_vendors,
+                      dont_filter=True)
+
+    def parse_lunch_vendors(self, response):
+        address = tools.get_address(response)
+        start_dtime, end_dtime = tools.get_time(response)
+
+        vendors_node_xp = "//h2[contains(text(), 'Lunch Shift')]/\
+parent::div/parent::div/following-sibling::div"
+
+        vendors = response.xpath(vendors_node_xp)[0]
+
+        for node in vendors.xpath(".//a[@class='summary-title-link']/text()"):
+            item = StreetFoodDatTimeItem()
+
+            item['VendorName'] = node.extract()
+            item['address'] = address
+            item['latitude'] = '37.770775'
+            item['longitude'] = '-122.391588'
+            item['start_datetime'] = start_dtime
+            item['end_datetime'] = end_dtime
+
+            yield item
+
+    def parse_dinner_vendors(self, response):
+        address = tools.get_address(response)
+        start_dtime, end_dtime = tools.get_time(response, dinner=True)
+
+        vendors_node_xp = "//h2[contains(text(), 'Dinner Shift')]/\
+parent::div/parent::div/following-sibling::div"
+
+        vendors = response.xpath(vendors_node_xp)[0]
+
+        for node in vendors.xpath(".//a[@class='summary-title-link']/text()"):
+            item = StreetFoodDatTimeItem()
+
+            item['VendorName'] = node.extract()
+            item['address'] = address
+            item['latitude'] = '37.770775'
+            item['longitude'] = '-122.391588'
+            item['start_datetime'] = start_dtime
+            item['end_datetime'] = end_dtime
+
+            yield item
---
+++ street_food/street_food/tools/sparksocialsf_tools.py
@@ -0,0 +1,79 @@
+import bs4
+import pytz
+from datetime import datetime
+
+
+def get_vendor_name(response):
+    try:
+        name_xp = ".//a[@class='summary-title-link']/text()"
+        name = response.xpath(name_xp).extract()[0]
+    except IndexError:
+        name = None
+
+    return name
+
+
+def get_address(response):
+    try:
+        addr_xp = "//h2[contains(text(), 'Location')]/following-sibling::p"
+        address = response.xpath(addr_xp).extract()[0]
+
+        soup = bs4.BeautifulSoup(address, "lxml")
+        address = soup.get_text(strip=True, separator=' ')
+
+    except IndexError:
+        address = None
+
+    return address
+
+
+def make_start_time(raw_time, cur_time):
+    time = raw_time.split("-")[0].strip() + "m"
+    vendor_stime = datetime.strptime(time, "%I:%M%p")
+    cur_time = cur_time.replace(hour=vendor_stime.hour,
+                                minute=vendor_stime.minute,
+                                second=0, microsecond=0)
+    return str(cur_time)
+
+
+def make_end_time(raw_time, cur_time):
+    time = raw_time.split("-")[1].strip() + "m"
+    vendor_stime = datetime.strptime(time, "%I:%M%p")
+    cur_time = cur_time.replace(hour=vendor_stime.hour,
+                                minute=vendor_stime.minute,
+                                second=0, microsecond=0)
+    print("CURTIME", cur_time)
+    return str(cur_time)
+
+
+def get_time(response, dinner=False):
+    tz = pytz.timezone("US/Pacific")
+    cur_time = datetime.now(tz=tz)
+
+    if cur_time.weekday() in range(5):
+        time_xp = "//h2[contains(text(), 'Weekdays')]/following-sibling::p/text()"
+        if dinner:
+            raw_time = response.xpath(time_xp).extract()[1]
+            start_time = make_start_time(raw_time, cur_time)
+            end_time = make_end_time(raw_time, cur_time)
+        else:
+            raw_time = response.xpath(time_xp).extract()[0]
+            start_time = make_start_time(raw_time, cur_time)
+            end_time = make_end_time(raw_time, cur_time)
+
+    elif cur_time.weekday() == 5:
+        time_xp = "//h2[contains(text(), 'Weekends')]/following-sibling::p/text()"
+        raw_time = response.xpath(time_xp).extract()[0]
+        start_time = make_start_time(raw_time, cur_time)
+        end_time = make_end_time(raw_time, cur_time)
+
+    elif cur_time.weekday() == 6:
+        time_xp = "//h2[contains(text(), 'Weekends')]/following-sibling::p/text()"
+        raw_time = response.xpath(time_xp).extract()[1]
+        start_time = make_start_time(raw_time, cur_time)
+        end_time = make_end_time(raw_time, cur_time)
+
+    else:
+        start_time = end_time = None
+
+    return start_time, end_time
